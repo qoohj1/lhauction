e2b025be767f2b67a6f86536f2719925cbbbee0f
qoohj1 <qoohj@qq.com>
2018-09-21 17:08:26 +0800

    up

--- website/www/config/database.php
+++ website/www/config/database.php
@@ -54,10 +54,10 @@ if($_SERVER['HTTP_HOST']=='dev.curio.com') {
     $db['default']['password'] = '';
     $db['default']['database'] = 'curio';
 } else {
-    $db['default']['hostname'] = '';
-    $db['default']['username'] = '';
+    $db['default']['hostname'] = '127.0.0.1:3306';
+    $db['default']['username'] = 'root';
     $db['default']['password'] = '';
-    $db['default']['database'] = '';
+    $db['default']['database'] = 'root';
 }
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
